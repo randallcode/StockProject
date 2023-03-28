d5f52633300aa9b98326e61644a26ec6e8b8cadb
Randy Egan <121986322+randallcode@users.noreply.github.comclear>
2023-03-28 18:49:10 -0400

    For the calculate function, I changed from var to let.

--- javascript.js
+++ javascript.js
@@ -5,12 +5,12 @@ function calculate()
         interestValue = document.getElementById("interestValue").value;
        
 
-            var interestValueDecimal = (interestValue/100) ;
-            var num1 = (interestValueDecimal/1); //0.040
-            var num2 = (1*yearsOfGrowth); //8
-            var num3 = (1+num1); //1.004
-            var num4 = (num3**num2); //1.3685
-            var result = "$"+(startingAmount * num4).toFixed(2);
+            let interestValueDecimal = (interestValue/100) ;
+            let num1 = (interestValueDecimal/1); 
+            let num2 = (1*yearsOfGrowth); 
+            let num3 = (1+num1); 
+            let num4 = (num3**num2); 
+            let result = "$"+(startingAmount * num4).toFixed(2);
             document.getElementById("result").innerHTML = result
             
             //alert("num1 is " + num1);
